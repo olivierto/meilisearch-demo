541c0c50e460ea62024d97ce91adb1939b2bc10e
Oliviertoto <olivier.tosello@gmail.com>
2022-10-19 13:37:08 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+
+const searchClient = instantMeiliSearch(
+  "https://integration-demos.meilisearch.com",
+  "99d1e034ed32eb569f9edc27962cccf90b736e4c5a70f7f5e76b9fab54d6a185"
+);
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "angular-app";
+  config = {
+    indexName: "steam-video-games",
+    searchClient
+  };
+}
